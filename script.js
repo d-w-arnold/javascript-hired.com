c57966f2cd91b376947d4c0ec70c69fa786f2053
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-04-24 02:21:44 +0100

    Added maze logic and learnt to use debugger

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 const nums = [7, 2, 6, 3];
 const list = [3, 6, 2, 9, -1, 10];
+const mze = [
+    [1,0,0,0,1],
+    [1,1,1,0,1],
+    [1,0,0,0,1],
+    [1,0,1,1,1],
+    [1,0,0,0,1]
+]
 
 function printMsg(id, fun) {
     document.getElementById(id).innerHTML = fun;
+}
+
+function mazePath(maze, startRow, startCol, destRow, destCol) {
+    var currentRow = startRow;
+    var currentCol = startCol;
+    var lastMove = -1;
+    while (true) {
+        if (currentRow === destRow && currentCol === destCol) {
+            return true;
+        }
+        if (((currentRow - 1) >= 0) && (maze[(currentRow - 1)][currentCol] === 0) && (lastMove !== 0)) { // Up
+            currentRow--;
+            lastMove = 2;
+            continue;
+        } else if (((currentCol + 1) >= 0) && (maze[currentRow][(currentCol + 1)] === 0) && (lastMove !== 1)) { // Right
+            currentCol++;
+            lastMove = 3;
+            continue;
+        } else if (((currentRow + 1) >= 0) && (maze[(currentRow + 1)][currentCol] === 0) && (lastMove !== 2)) { // Down
+            currentRow++;
+            lastMove = 0;
+            continue;
+        } else if (((currentCol - 1) >= 0) && (maze[currentRow][(currentCol - 1)] === 0) && (lastMove !== 3)) { // Left
+            currentCol--;
+            lastMove = 1;
+            continue;
+        }
+        return false;
+    }
 }
 
 function largestNum(numbers) {
